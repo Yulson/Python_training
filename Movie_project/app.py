79b2b491e8adad4c583a0a2e5e1e048ac8859409
Yulson <iuliia.baranova@gmail.com>
2019-04-12 00:18:31 -0400

    Adding the scope of tasks to be done within the project

---
+++ Movie_project/app.py
@@ -0,0 +1,19 @@
+"""
+Is to create a movie storage application, that will allow users to manage their movie collection and  nd any movie they want.
+Here’s the three main features:
+• First, the application must allow to add new movies to the collection;
+• The application must allow users to view all the movies in their collection;
+• The application must also allow users to find any particular movie by any of its attrib- utes (more info in the next page...)
+"""
+"""
+List of tasks:
+1. Allow user to chose from the menu: add new movie, view all the movies in collection, find a particular movie.
+2. User should be able to add a movie
+3. User should be able to view the complete list of all added movies
+4. User should be able to find a particular movie by name.
+5. User should be able to quit the menu.
+"""
+
+
+def menu()
+ input
